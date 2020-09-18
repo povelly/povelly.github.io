fe5c7df4c8d6fdf610764a21f4cab60b53d3db80
povelly <40063601+povelly@users.noreply.github.com>
2020-09-18 14:24:59 +0200

    Resume Upload

---
+++ scripts/translation.js
@@ -0,0 +1,121 @@
+var translation = {
+  "_POSITION_": {
+    fr: "Ingénieur logiciel",
+    en: "Software engineer" },
+  "_SUBTITLE_": {
+    fr: "Curriculum Vitæ",
+    en: "Resume" },
+  "_SKILLS_": {
+    fr: "Compétences",
+    en: "Skills" },
+  "_PROGRAMMING_": {
+    fr: "Langages et technologies",
+    en: "Programming and technologies" },
+  "_DATABASE_": {
+    fr: "Bases de données (SQL, MongoDB)",
+    en: "Database (SQL, MongoDB)" },
+  "_notions_prog": {
+    fr: "Notions de Go, C, OCaml, Prolog, LaTeX",
+    en: "Notions of Go, C, OCaml, Prolog, LaTeX" },
+  "_OS_": {
+    fr: "Systèmes d'exploitation",
+    en: "Operating systems" },
+  "_TOOLS_": {
+    fr: "Outils",
+    en: "Tools" },
+  "_OFFICE_": {
+    fr: "Suite Office (et équivalent)",
+    en: "Office suites" },
+  "_GRAPHICS_": {
+    fr: "Notions sur PhotoShop et Paint.NET",
+    en: "Notions about PhotoShop and Paint.NET" },
+  "_LANGUAGE_": {
+    fr: "Langues",
+    en: "Languages" },
+  "_FR_": {
+    fr: "Français",
+    en: "French" },
+  "_EN_": {
+    fr: "Anglais",
+    en: "English" },
+  "_GE_": {
+    fr: "Allemand",
+    en: "German" },
+  "_LA_": {
+    fr: "Latin",
+    en: "Latin" },
+  "_GR_": {
+    fr: "Grec",
+    en: "Greek" },
+  "_FRLV_": {
+    fr: "langue maternelle",
+    en: "mother tongue" },
+  "_ENLV_": {
+    fr: "niveau B1",
+    en: "B1 level" },
+  "_GELV_": {
+    fr: "DSD I B1",
+    en: "DSD I B1" },
+  "_LALV_": {
+    fr: "6 ans",
+    en: "6 years" },
+  "_GRLV_": {
+    fr: "2 ans",
+    en: "2 years" },
+  "_ABOUT_": {
+    fr: "À mon sujet",
+    en: "About me" },
+  "_BIO_": {
+    fr: "Étudiant en deuxième année de master informatique motivé et perfectionniste, à 21 ans je suis à la recherche d'un " + "stage".bold() + " de fin d'études d'une durée de " + "6 mois".bold() + " dans le domaine du " + "logiciel".bold() + ".",
+    en: "21 years old french student in computer science Master degree, I am currently looking for a " + "6-month".bold() + " end of studies " + "software internship".bold() + "." },
+  "_FORMATION_": {
+    fr: "Formation",
+    en: "Formation" },
+  "_LEG_": {
+    fr: "Licence Économie & Gestion",
+    en: "Degree in Economics and Management" },
+  "_MINFO_": {
+    fr: "Master Informatique",
+    en: "Master's degree in Computer Science" },
+  "_MDESC_": {
+    fr: "Parcours Science et Technologie du Logiciel (STL)",
+    en: "Software engineering" },
+  "_LINFO_": {
+    fr: "Licence Informatique",
+    en: "Bachelor's degree in computer science" },
+  "_PREPA_": {
+    fr: "Prépa MPSI",
+    en: "Preparatory class for high scientific school" },
+  "_BAC_": {
+    fr: "Baccalauréat S",
+    en: "High School diploma, scientific option" },
+  "_BACDESC_": {
+    fr: "Mention bien",
+    en: "With honours" },
+  "_EXPERIENCE_": {
+    fr: "Expériences",
+    en: "Professional experiences" },
+  "_TUTORING_": {
+    fr: "Soutien scolaire",
+    en: "Tutoring" },
+  "_HOME_": {
+    fr: "à domicile",
+    en: "at home" },
+  "_TUTORINGDESC_": {
+    fr: "Mathématiques niveau collège et lycée",
+    en: "Mathematics, middle and high school level" },
+  "_INTERNSHIP_": {
+    fr: "Stage de 3" + "e".sup(),
+    en: "3" + "e".sup() + " internship" },
+  "_ALCATEL_": {
+    fr: "Alcatel-Lucent",
+    en: "Alcatel-Lucent" },
+  "_INTERNSHIPDESC_": {
+    fr: "Pour une durée d'une semaine",
+    en: "For a week" },
+  "_HOBBY_": {
+    fr: "Centres d'intérêt",
+    en: "Hobbies" }
+};
+
+var translator = $('body').translate({lang: "fr", t: translation});
